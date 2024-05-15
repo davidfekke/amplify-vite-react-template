631671529d1d9f81005cf7144b25e946c8a6d971
David Fekke <david@fekke.com>
2024-05-14 22:25:53 -0400

    Removing tutorial link

--- src/App.tsx
+++ src/App.tsx
@@ -34,9 +34,6 @@ function App() {
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
-        <a href="https://next-release-dev.d1ywzrxfkb9wgg.amplifyapp.com/react/start/quickstart/vite-react-app/#step-2-add-delete-to-do-functionality">
-          Review next step of this tutorial.
-        </a>
       </div>
     </main>
   );
